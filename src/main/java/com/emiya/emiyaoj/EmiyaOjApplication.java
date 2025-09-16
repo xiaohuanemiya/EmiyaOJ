3021c818c0a78ce12ba33e9d2c8b276e11ed87c0
xiaohuanemiya <emiya172625@gmail.com>
2025-09-16 19:15:05 +0800

    first rush

---
+++ src/main/java/com/emiya/emiyaoj/EmiyaOjApplication.java
@@ -0,0 +1,13 @@
+package com.emiya.emiyaoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiyaOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmiyaOjApplication.class, args);
+    }
+
+}
