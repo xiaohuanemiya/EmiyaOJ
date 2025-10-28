d14437d90b11d30a85ec497a8ee1b3455561ec1f
Er1da <2738415794@qq.com>
2025-10-28 20:39:34 +0800

    博客模块数据库初始化

---
+++ database/blog_destroy.sql
@@ -0,0 +1,9 @@
+use `emiya-oj`;
+
+drop table if exists user_blog;
+
+drop table if exists blog;
+
+drop table if exists blog_tag;
+
+drop table if exists blog_tag_association;
---
+++ oj-service/src/main/java/com/emiyaoj/service/controller/BlogController.java
@@ -0,0 +1,10 @@
+package com.emiyaoj.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+    
+}
