ce37939999000e91978d2e700dd9047ce43a0aa7
xiaohuanemiya <emiya172625@gmail.com>
2025-12-16 20:38:44 +0800

    修改地址

--- oj-service/src/main/java/com/emiyaoj/service/util/GoJudgeService.java
+++ oj-service/src/main/java/com/emiyaoj/service/util/GoJudgeService.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Service
 public class GoJudgeService {
     
-    @Value("${gojudge.url:http://101.34.220.54:12812}")
+    @Value("${gojudge.url:http://127.0.0.1:12812}")
     private String goJudgeUrl;
     
     private final RestTemplate restTemplate;
